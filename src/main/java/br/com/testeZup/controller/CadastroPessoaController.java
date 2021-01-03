9b9870f95161e5eef51b137e8ff7dfd313c47c9b
Ludmila Araujo <silveira.ludmila@yahoo.com.br>
2021-01-03 09:28:47 -0300

    Melhoria na Controller

--- src/main/java/br/com/testeZup/controller/CadastroPessoaController.java
+++ src/main/java/br/com/testeZup/controller/CadastroPessoaController.java
@@ -1,5 +1,6 @@
 package br.com.testeZup.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -7,6 +8,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,5 +54,6 @@ public class CadastroPessoaController {
 		}
 		
 	}
+
 }
  
